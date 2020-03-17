a85847bec707a92f1de7c757ac4df7e2f88a068b
tstassin <tstassin@gmail.com>
2020-03-17 07:12:54 +0100

    Get covid19 data available on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
+    {console.log(data)}
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Covid19 Status in Belgium</h1>
+    
+    <ul>
+      {data.allData.edges.map(data => <li>{data.node.date}</li>)}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+query MyQuery {
+  allData {
+    edges {
+      node {
+        cumul_cases
+        cumul_tests
+        daily_cases
+        daily_tests
+        icu
+        date
+        deceased
+        hospitalized
+      }
+    }
+  }
+}
+`
