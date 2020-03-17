ff7d9e0977edbe774c84a4cb3c8163146d12b32b
tstassin <tstassin@gmail.com>
2020-03-17 09:44:34 +0100

    Customize site metadata, remove defaults from starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Covid19 Status in Belgium`,
+    description: `Charting relevant Covid19 data in Belgium to help people further understand the situation and raise awareness`,
+    author: `@tstassin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-cov19`,
+        short_name: `cov19`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
